6a0ac3f3a960bd666265ea26112647793bd07411
danibarboza2014 <38190347+danibarboza2014@users.noreply.github.com>
2025-05-20 16:00:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-z# app.py
 from machine import Pin
 import time
+
+led = Pin(2, Pin.OUT)
 
 print("🚀 Aplicación iniciada")
 
 def run():
     while True:
-        print("✅ Ejecutando Modificado Diezzz..")
+        print("Ejecutando  Cambio 1111...")  
         time.sleep(1)
